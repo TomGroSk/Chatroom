dc0078edf0f6433df7af1b93591e16dae244a309
TomGroSk <44368592+TomGroSk@users.noreply.github.com>
2019-04-12 23:23:50 +0200

    Add server
    Server is now working fine

--- src/com/company/Server.java
+++ src/com/company/Server.java
@@ -21,7 +21,7 @@ public class Server extends JFrame{
         userText.addActionListener(
                 new ActionListener() {
                     public void actionPerformed(ActionEvent e) {
-                        //sendMessage(e.getActionCommand());
+                        sendMessage(e.getActionCommand());
                         userText.setText("");
                     }
                 }
@@ -39,14 +39,14 @@ public class Server extends JFrame{
             while (true){
                 try{
                     waitForConnection();
-                    //setUpStreams();
-                    //whileChatting();
+                    setUpStreams();
+                    whileChatting();
                 }
                 catch (EOFException eofExcept){
-                    //showMessage("\n Server ended the connection! ");
+                    showMessage("\n Server ended the connection! ");
                 }
                 finally {
-                    //closeCrap();
+                    closeCrap();
                 }
             }
         }
@@ -56,8 +56,68 @@ public class Server extends JFrame{
     }
 
     private void waitForConnection() throws IOException{
-        //showMessage(" Waiting for someone to connect . . . \n ");
+        showMessage(" Waiting for someone to connect . . . \n ");
         connection = server.accept();
+        showMessage(" Connected to: "+connection.getInetAddress().getHostName());
     }
-
+    private void setUpStreams() throws IOException{
+        output = new ObjectOutputStream(connection.getOutputStream());
+        output.flush();
+        input = new ObjectInputStream(connection.getInputStream());
+        showMessage("\n Streams are setup now! ");
+    }
+    private void whileChatting() throws IOException{
+        String message = "You are now connected!";
+        sendMessage(message);
+        ableToType(true);
+        do{
+            try{
+                message = (String) input.readObject();
+                showMessage("\n"+message);
+            }
+            catch (ClassNotFoundException clfExcept){
+                showMessage(" \n I don't know what that user send!");
+            }
+        }while(!message.equals("CLIENT - END"));
+    }
+    private void closeCrap(){
+        showMessage("\n Closing connections . . . \n");
+        ableToType(false);
+        try{
+            output.close();
+            input.close();
+            connection.close();
+        }
+        catch (IOException ioExcept){
+            ioExcept.printStackTrace();
+        }
+    }
+    private void sendMessage(String message){
+        try{
+            output.writeObject("SERVER: "+ message);
+            output.flush();
+            showMessage("\nSERVER: "+ message);
+        }
+        catch (IOException ioExcept){
+            chatWindow.append("\n  Ooops can't send that message!");
+        }
+    }
+    private void showMessage(final String text){
+        SwingUtilities.invokeLater(
+            new Runnable() {
+                public void run() {
+                    chatWindow.append(text);
+                }
+            }
+        );
+    }
+    private void ableToType(final boolean decide){
+        SwingUtilities.invokeLater(
+                new Runnable() {
+                    public void run() {
+                        userText.setEditable(decide);
+                    }
+                }
+        );
+    }
 }
